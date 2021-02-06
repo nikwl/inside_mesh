27a3a3b533c4ae2ee4478c839016405b9c6c1987
nikolas <nikolas.lamb@gmail.com>
2021-02-06 17:08:23 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import numpy
+
+# triangle hash (efficient mesh intersection)
+triangle_hash_module = Extension(
+    'libmesh/triangle_hash',
+    sources=[
+        'libmesh/triangle_hash.pyx'
+    ],
+    include_dirs=[numpy.get_include()],
+    libraries=['m']  # Unix-like specific
+)
+
+setup(
+    name='libmesh',
+    ext_modules=cythonize([triangle_hash_module]),
+    packages=['libmesh']
+)
