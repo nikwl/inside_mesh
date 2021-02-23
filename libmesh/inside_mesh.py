e95ad8bd9b41ceb25ba814255010f5983a935cff
nikolas <nikolas.lamb@gmail.com>
2021-02-23 07:33:56 -0500

    contains1 != contains2 now raises a runtime error

--- libmesh/inside_mesh.py
+++ libmesh/inside_mesh.py
@@ -68,7 +68,7 @@ class MeshIntersector:
         contains1 = (np.mod(nintersect0, 2) == 1)
         contains2 = (np.mod(nintersect1, 2) == 1)
         if (contains1 != contains2).any():
-            print('Warning: contains1 != contains2 for some points.')
+            raise RuntimeError('contains1 != contains2 for some points')
         contains[mask] = (contains1 & contains2)
         return contains
 
